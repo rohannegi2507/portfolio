c112cb21299d2f13edfa64350e16c4d9821983dd
rohan <rohan.negi.dev@gmail.com>
2022-10-24 17:14:10 +0530

    url fix

--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,7 +1,7 @@
 import { Project } from "../typing";
 
 export const fetchProjects = async()=>{
-  const res =  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`)   
+  const res =  await fetch(`https://rohannegi.com/api/getProjects`)   
   const data = await res.json()
   const projects: Project[]= data.projects  
   return projects
