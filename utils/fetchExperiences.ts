026378aa638922bc230c207e47a4aa9f5c28d78d
rohan <rohan.negi.dev@gmail.com>
2022-11-01 00:21:38 +0530

    env update

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,7 +1,7 @@
 import { Experience } from "../typing";
 
 export const fetchExperiences = async()=>{
-  const res =  await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`)   
+  const res =  await fetch('https/rohannegi.com/api/getExperiences')
   const data = await res.json()
   const experiences: Experience[]= data.experiences  
   return experiences
